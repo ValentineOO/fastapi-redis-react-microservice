bbd37e83dd41dccc52323105fb5fa44f61d4729c
Valentine OO <valoladimeji@gmail.com>
2025-06-10 07:20:39 +0100

    feat(payment): add order creation with Redis storage and external product lookup

--- payment/main.py
+++ payment/main.py
@@ -5,11 +5,15 @@ from pathlib import Path
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests
 
 
 # Load the environment variables from payment-specific file
 env_path = Path(__file__).parent / '.env.payment'
 load_dotenv(dotenv_path=env_path)
+PRODUCT_SERVICE_URL = os.environ.get(
+    "PRODUCT_SERVICE_URL", "http://localhost:8000")
 
 app = FastAPI()
 
@@ -20,6 +24,7 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+# This should be a different database
 redis = get_redis_connection(
     host=os.environ["REDIS_HOST"],
     port=int(os.environ["REDIS_PORT"]),
@@ -28,3 +33,41 @@ redis = get_redis_connection(
     decode_responses=True,
 )
 
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str  # Pending, completed, refunded
+
+    class Meta:
+        database = redis
+
+
+@app.post('/orders')
+async def create(request: Request):  # id, quantity
+    body = await request.json()
+
+    req = requests.get(f'{PRODUCT_SERVICE_URL}/products/{body["id"]}')
+    product = req.json()
+
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+
+    order.save()
+
+    order_completed(order)
+    return order
+
+
+def order_completed(order: Order):
+    order.status = 'completed'
+    order.save()
